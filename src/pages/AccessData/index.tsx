7c9721a58362efd62bf7861c417612b66d783b95
Filipe Sales Araujo <filipesalesaraujo@gmail.com>
2022-02-18 14:32:26 -0300

    projeto base

---
+++ src/pages/AccessData/index.tsx
@@ -0,0 +1,19 @@
+import Header from "../../components/Header";
+import { AnalyticsDataContextProvider } from "../../context/AnalyticsData/indext";
+import * as S from "./styles";
+import * as C from "./Components";
+
+const AccessData = () => (
+    <AnalyticsDataContextProvider>
+        <Header />
+        <S.Content>
+            <C.TotalAccessOnPages />
+            <C.PorcentageOfAccessOnPages />
+            <C.TotalAccessPerHour />
+            <C.TotalAccessPerMonth />
+            <C.WordCloud />
+        </S.Content>
+    </AnalyticsDataContextProvider>
+);
+
+export default AccessData;
